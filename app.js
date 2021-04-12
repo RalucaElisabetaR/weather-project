6f93aa3544a869207bd60452829efdefd62ff3d2
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-12 22:33:45 +0200

    render live API data with express

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
+// jshint esversion:6
+
 const express = require('express')
 const https = require('https')
+
+const bodyParser = require('body-parser')
+
 const app = express()
-app.get('/', (__request, response) => {
-    const url =
-        'https://api.openweathermap.org/data/2.5/weather?APPID=a63307ba1cc6efe973291d68a210e13a&q=Tervuren&units=metric'
-    https.get(url, (response) => {
-        console.log(response.statusCode)
-        response.on('data', (data) => {
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            console.log(temp, weatherDescription)
-        })
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
     })
-    response.send('Server is up and running.')
+)
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000.')
+app.post('/', function (req, res) {
+    console.log(req.body.cityName)
+
+    const query = req.body.cityName
+
+    const appKey = 'a63307ba1cc6efe973291d68a210e13a'
+
+    const unit = 'metric'
+
+    const url =
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        query +
+        '&appid=' +
+        appKey +
+        '&units=' +
+        unit
+
+    https.get(url, function (response) {
+        console.log(response.statusCode)
+
+        response.on('data', function (data) {
+            const weatherData = JSON.parse(data)
+
+            const temp1 = weatherData.main.temp
+
+            const weather = weatherData.weather[0].description
+
+            const icon = weatherData.weather[0].icon
+
+            const imageUrl =
+                'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+            console.log(weather)
+
+            res.write('<h1> The Weather is currently ' + weather + '</h1>')
+
+            res.write(
+                '<br><h1>The temperature in ' +
+                    query +
+                    ' is ' +
+                    temp1 +
+                    ' degree Celcius</h1>'
+            )
+
+            res.write(`<img src ='` + imageUrl + `'>`)
+
+            res.send()
+        })
+    })
+})
+
+app.listen(5000, function () {
+    console.log('Server is running on 5000 ')
 })
